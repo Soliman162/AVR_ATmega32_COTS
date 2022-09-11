3b8a1f1ff2e88d21b5c2d8934054575b271ca3dc
Soliman162 <ahmedsliman215@gmail.com>
2022-09-11 21:17:54 +0200

    ATmega32_COTS

--- Test/main.c
+++ Test/main.c
@@ -7,38 +7,10 @@
 #include "HC05_interface.h"
 #include "LED_interface.h"
 
-#include "wheel_chair.h" 
-
 int main(void)
 {
-    MOVING_STATE Current_enumDierction = STOP ;
-    MOVING_STATE Previous_enumDirection = STOP;
-
-    HC05_voidInit();
-    WheelChair_voidInit();
-
     while(1)
     {
-
-       while( Current_enumDierction == Previous_enumDirection ){ Current_enumDierction = HC05_u8Receive(); }
-
-       if( Current_enumDierction == STOP )
-       {
-            Wheel_Chair_voidStop();
-       }else if( Current_enumDierction == FORWARD )
-       {
-           WheelChair_voidMove_Forward();
-       }else if ( Current_enumDierction == BACKWARD )
-       {
-           WheelChair_voidMove_Backward();
-       }else if( Current_enumDierction == RIGHT )
-       {
-           WheelChair_voidMove_Right();
-       }else if( Current_enumDierction == LEFT )
-       {
-           WheelChair_voidMove_Left();
-       }
-       Previous_enumDirection = Current_enumDierction;
 
     }
 
---
+++ build/CMakeFiles/ATmega32_COTS.elf.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/soliman/AVR/ATmega32/AVR_COTS/HAL/CLCD/CLCD_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/CLCD/CLCD_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/CLCD/CLCD_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/HAL/HC_05/HC05_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/HC_05/HC05_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/HC_05/HC05_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/HAL/L298/L298_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/L298/L298_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/L298/L298_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LDR/LDR_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LDR/LDR_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LDR/LDR_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LED/LED_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LED/LED_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LED/LED_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LM35/LM35_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LM35/LM35_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/HAL/LM35/LM35_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/01-DIO/DIO_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/01-DIO/DIO_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/01-DIO/DIO_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/02-UART/UART_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/02-UART/UART_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/02-UART/UART_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/03-ADC/ADC_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/03-ADC/ADC_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/03-ADC/ADC_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/04-Timer1/TIMER1_program.c" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/04-Timer1/TIMER1_program.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/home/soliman/AVR/ATmega32/AVR_COTS/MCAL/04-Timer1/TIMER1_program.c.o.d"
+  "/home/soliman/AVR/ATmega32/AVR_COTS/Test/main.c" "CMakeFiles/ATmega32_COTS.elf.dir/main.c.o" "gcc" "CMakeFiles/ATmega32_COTS.elf.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
