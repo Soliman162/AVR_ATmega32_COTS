79c8e074100ae6abc856ab49a9330b1bda1a0382
Soliman162 <ahmedsliman215@gmail.com>
2022-09-20 00:15:25 +0200

    Timer1 Edit

--- MCAL/04-Timer1/TIMER1_interface.h
+++ MCAL/04-Timer1/TIMER1_interface.h
@@ -3,11 +3,10 @@
 
 typedef enum{
 
-
-    TIMER1_CAPT=0,
+    TIMER1_OVF = 2,
+    TIMER1_COMPB, 
     TIMER1_COMPA,
-    TIMER1_COMPB,   
-    TIMER1_OVF
+    TIMER1_CAPT
 
 }TIMER1_INTERRUPT_INDEX;
 
@@ -16,12 +15,10 @@ typedef enum{
 void Timer1_voidInit(void);
 
 void Timer1_voidEnableOVERFLOW_INTERRUPT(void);
-
+void Timer1_voidDisableOVERFLOW_INTERRUPT(void);
 void Timer1_voidDelay_Micro_Seconds(u32 Copy_u32Delay_micro_Seconds);
-
 void Timer1_voidSet_Call_Back(TIMER1_INTERRUPT_INDEX Copy_enumInterrupt, void *Copy_ISR(void));
-
-void Timer1_voidClearFlag(void);
+void Timer1_voidClear_OVERFLOW_Flag(void);
 
 
 
--- MCAL/04-Timer1/TIMER1_program.c
+++ MCAL/04-Timer1/TIMER1_program.c
@@ -18,6 +18,11 @@ void Timer1_voidEnableOVERFLOW_INTERRUPT(void)
     SETBIT(TIMSK_REG,TIMER1_OVERFLOW_INT_Enable_BIT_NUM);
 }
 
+void Timer1_voidDisableOVERFLOW_INTERRUPT(void)
+{
+    CLRBIT(TIMSK_REG,TIMER1_OVERFLOW_INT_Enable_BIT_NUM);
+}
+
 void Timer1_voidDelay_Micro_Seconds(u32 Copy_u32Delay_micro_Seconds)
 {
     while (Copy_u32Delay_micro_Seconds > TIMER1_OVER_FLOW_VALUE)
@@ -45,49 +50,47 @@ void Timer1_voidDelay_Micro_Seconds(u32 Copy_u32Delay_micro_Seconds)
 
 void Timer1_voidSet_Call_Back(TIMER1_INTERRUPT_INDEX Copy_enumInterrupt, void *Copy_ISR(void));
 {
-    Timer1_ISR[Copy_enumInterrupt] =  Copy_ISR;
+    Timer1_ISR[Copy_enumInterrupt-2] =  Copy_ISR;
 }
 
-void Timer1_voidClearFlag(void)
+void Timer1_voidClear_OVERFLOW_Flag(void)
 {
     SETBIT(TIFR_REG,TIMER1_OVERFLOW_FLAG_BIT_NUM);
 }
 
 
-
 /*Timer 1 CAPT ISR*/
 void __vector_6 (void)
 {
-    if( Timer1_ISR[TIMER1_OVF] != NULL )
+    if( Timer1_ISR[TIMER1_CAPT-2] != NULL )
     {
-        Timer1_ISR[TIMER1_OVF]();
+        Timer1_ISR[TIMER1_CAPT-2]();
     }  
 }
 
 /*Timer 1 COMPA ISR*/
 void __vector_7 (void)
 {
-    if( Timer1_ISR[TIMER1_OVF] != NULL )
+    if( Timer1_ISR[TIMER1_COMPA-2] != NULL )
     {
-        Timer1_ISR[TIMER1_OVF]();
+        Timer1_ISR[TIMER1_COMPA-2]();
     }  
 }
 
 /*Timer 1 COMPB ISR*/
 void __vector_8 (void)
 {
-    if( Timer1_ISR[TIMER1_OVF] != NULL )
+    if( Timer1_ISR[TIMER1_COMPB-2] != NULL )
     {
-        Timer1_ISR[TIMER1_OVF]();
+        Timer1_ISR[TIMER1_COMPB-2]();
     }  
 }
-
 
 /*Timer 1 OVF ISR*/
 void __vector_9 (void)
 {
-    if( Timer1_ISR[TIMER1_OVF] != NULL )
+    if( Timer1_ISR[TIMER1_OVF-2] != NULL )
     {
-        Timer1_ISR[TIMER1_OVF]();
+        Timer1_ISR[TIMER1_OVF-2]();
     }  
 }
