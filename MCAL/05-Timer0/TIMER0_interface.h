7095557caca43e2654a67e1440bf670db39072d5
Soliman162 <ahmedsliman215@gmail.com>
2022-10-10 01:13:57 +0200

    ADD_TIMER0_PWM

--- MCAL/05-Timer0/TIMER0_interface.h
+++ MCAL/05-Timer0/TIMER0_interface.h
@@ -6,5 +6,10 @@ void Timer0_voidInit(void);
 
 void Timer0_voidDelay_us(u32 Copy_u32DelayTime);
 
+void Timer0_voidSet_Duty_Cycle( u8 Copy_u8Duty_Cycle );
+
+
+
+
 
 #endif
--- MCAL/05-Timer0/TIMER0_program.c
+++ MCAL/05-Timer0/TIMER0_program.c
@@ -5,11 +5,13 @@
 
 #include "TIMER0_interface.h"
 #include "TIMER0_private.h"
+#include "TIMER0_config.h"
 
 void Timer0_voidInit(void)
 {
     /*OVF mode - prescaler 8*/
-    TCCR0_REG = 0b00000010;
+    //TCCR0_REG = 0b00000010;
+    TCCR0_REG = TIMER0_MODE | OC0_MODE | PRESCALLER_VALUE ;
 }
 
 void Timer0_voidDelay_us(u32 Copy_u32DelayTime)
@@ -34,4 +36,33 @@ void Timer0_voidDelay_us(u32 Copy_u32DelayTime)
         /*CLR the Flag */
         SETBIT(TIFR_REG,TIMER0_OVF_FLAG_BIT);
     }
+}
+
+void Timer0_voidSet_Duty_Cycle( u8 Copy_u8Duty_Cycle )
+{
+    #if TIMER0_MODE == FAST_PWM
+
+        #if OC0_MODE == CLEAR_ON_COMPARE    /*NON_INVERTED*/
+
+            OCR0_REG = ((Copy_u8Duty_Cycle*256)/100)-1;
+
+        #elif  OC0_MODE ==                  /*INVERTED*/
+
+            OCR0_REG = 255-((Copy_u8Duty_Cycle*256)/100);   
+
+        #endif
+
+    #elif  TIMER0_MODE == PHASE_CORRECT_PWM
+
+        #if OC0_MODE == CLEAR_ON_COMPARE    /*NON_INVERTED*/
+
+            OCR0_REG = ((Copy_u8Duty_Cycle*255)/100) ;
+
+        #elif  OC0_MODE ==                  /*INVERTED*/
+
+            OCR0_REG = 255-((Copy_u8Duty_Cycle*255)/100) ;  
+
+        #endif
+
+    #endif
 }
